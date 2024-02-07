3f9ed9b38e5359d02abba6ecbc48400f50f76d7e
“pradeeppatole” <pradeep.patole@gslab.com>
2024-02-07 08:38:07 +0530

    PY Tasks commit

---
+++ PyAutoTasks/Task1.py
@@ -0,0 +1,11 @@
+name= input("Please enter your name: ")
+age= input("Please enter your age: ")
+roll_number= input("Please enter you roll number: ")
+phone_number= input("Please enter your phone number: ")
+print("Thank you for the input, we will now re-confirm your details")
+
+print("You have entered name as: ", name)
+print("You have entered age as: ", age)
+print("You have entered roll number as: ", roll_number)
+print("You have entered phone number as: ", phone_number)
+
---
+++ PyAutoTasks/Task2.py
@@ -0,0 +1,17 @@
+print(2,"x",1,"=",2)
+print(2,"x",2,"=",4)
+print(2,"x",3,"=",6)
+print(2,"x",4,"=",8)
+print(2,"x",5,"=",10)
+print(2,"x",6,"=",12)
+print(2,"x",7,"=",14)
+print(2,"x",8,"=",16)
+print(2,"x",9,"=",18)
+print(2,"x",10,"=",20)
+
+# using a for loop
+
+
+a=2
+for i in range(1,11):
+    print(a, "x", i, "=",a*i)
